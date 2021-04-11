3f12d34f2f00c95cb20182bfc4d7a464750ad6d3
Ashutosh Kushawaha <akushawaha@qkly.io>
2021-04-11 18:21:42 +0545

    opp concept is reviewed

---
+++ ReviewCode/OOPReview/StudentLibraryInfo.cs
@@ -0,0 +1,50 @@
+﻿using ReviewCode.OOPReview.StudentInfo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReviewCode.OOPReview
+{
+    public class StudentLibraryInfo
+    {
+        public bool SaveStudentInfo(Student stdObj)
+        {
+            try
+            {
+                if (stdObj.FirstName == "Ram")
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public List<Student> SaveBulkStudentInfo(List<Student> stdListObj)
+        {
+            try
+            {
+                foreach (var item in stdListObj)
+                {
+                    if (item.FirstName == "Ram")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                return stdListObj;
+            }
+            catch (Exception ex)
+            {
+                return stdListObj;
+            }
+        }
+    }
+}
--- ReviewCode/Program.cs
+++ ReviewCode/Program.cs
@@ -1,36 +1,62 @@
 ﻿using ReviewCode.CalculateFactorial;
 using ReviewCode.CheckAge;
 using ReviewCode.EvenOddTest;
+using ReviewCode.OOPReview;
+using ReviewCode.OOPReview.StudentInfo;
 using System;
+using System.Collections.Generic;
 
 namespace ReviewCode
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //This is for even odd print
-            EvenOdd obj = new EvenOdd();
-            obj.CalculateEvenOdd();
-            //This is for calculate age
-            //CalculateAge age = new CalculateAge();
-            //Console.WriteLine("enter age");
-            //int ages =int.Parse( Console.ReadLine());
-            //var actualagename = age.Calulate(ages);
-            //Console.WriteLine("you are " + actualagename);
-            //Console.ReadLine();
-            //calculate factorial
-            CalculateFacat facObj = new CalculateFacat();
-            Console.WriteLine("enter a number");
-            int FactNum = int.Parse(Console.ReadLine());
-           var results= facObj.FactRecursive(FactNum);
-            Console.WriteLine(results);
+            // Console.WriteLine("Hello World!");
+            // //This is for even odd print
+            // EvenOdd obj = new EvenOdd();
+            // obj.CalculateEvenOdd();
+            // //This is for calculate age
+            // //CalculateAge age = new CalculateAge();
+            // //Console.WriteLine("enter age");
+            // //int ages =int.Parse( Console.ReadLine());
+            // //var actualagename = age.Calulate(ages);
+            // //Console.WriteLine("you are " + actualagename);
+            // //Console.ReadLine();
+            // //calculate factorial
+            // CalculateFacat facObj = new CalculateFacat();
+            // Console.WriteLine("enter a number");
+            // int FactNum = int.Parse(Console.ReadLine());
+            //var results= facObj.FactRecursive(FactNum);
+            // Console.WriteLine(results);
+            // Console.ReadLine();
+            StudentLibraryInfo sliObj = new StudentLibraryInfo();
+            Student studentObj = new Student();
+            studentObj.FirstName = "Ram";
+            studentObj.LastName = "Kumar";
+            studentObj.Email = "apk@email.com";
+            studentObj.RollNo = 2;
+            studentObj.Id = 1;
+            studentObj.Address = "KTM";
+            // var result = sliObj.SaveStudentInfo(studentObj);
+
+            //Creating list of student
+            List<Student> stdListObj = new List<Student>();
+            Student studentObj2 = new Student();
+            studentObj2.FirstName = "Shyam";
+            studentObj2.LastName = "Kumar";
+            studentObj2.Email = "apk@email.com";
+            studentObj2.RollNo = 1;
+            studentObj2.Id = 2;
+            studentObj2.Address = "KTM";
+            stdListObj.Add(studentObj);
+            stdListObj.Add(studentObj2);
+            stdListObj.Add(studentObj);
+            stdListObj.Add(studentObj2);
+            stdListObj.Add(studentObj);
+            stdListObj.Add(studentObj2);
+            var res = sliObj.SaveBulkStudentInfo(stdListObj);
             Console.ReadLine();
-
-
-
-
         }
-}
+    }
 }
